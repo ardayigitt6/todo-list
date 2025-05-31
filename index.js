3e4a48f185cca51f8b9828842e324c124c0a3b76
Arda Yiğit <arda.yigit.5811@gmail.com>
2025-05-31 23:23:14 +0300

    todo API eklendi.

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-app.get('/', (req, res) => {
-res.send('merhaba,to-do backend çalışıyor.');
+let todos=[];
+app.get('/todos', (req,res) => {
+res.json(todos);
 });
-app.listen(3000, () => {
+app.post('/todos',(req,res) => {
+const {title} = req.body;
+if (!title) {
+return res.status(400).json({ error:'Başlık gerekli!'}) ;
+}
+const newTodo = {id: Date.now(), title: title };
+todos.push(newTodo);
+res.status(201).json(newTodo);
+});
+app.get('/',(req,res) => {
+res.send('Merhaba, to-do backend çalışıyor.');
+});
+app.listen(300, () => {
 console.log('Sunucu 3000 portunda çalışıyor.');
 });
+
+
+
+

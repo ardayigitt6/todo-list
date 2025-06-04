6b1c2ca5531500be8ff0f3fb446f508f1f05a1db
Arda Yiğit <arda.yigit.5811@gmail.com>
2025-06-05 02:24:02 +0300

    Görev ekleme ve listeleme eklendi

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-let todos=[];
-app.get('/todos', (req,res) => {
+let todos = [];
+app.get('/todos', (req, res) => {
 res.json(todos);
 });
-app.post('/todos',(req,res) => {
-const {title} = req.body;
+app.post('/todos', (req, res) => {
+const { title } = req.body;
 if (!title) {
-return res.status(400).json({ error:'Başlık gerekli!'}) ;
+return res.status(400).json({ error:'Başlık gerekli !'});
 }
-const newTodo = {id: Date.now(), title: title };
+const newTodo = { id: Date.now(), title: title };
 todos.push(newTodo);
 res.status(201).json(newTodo);
 });
-app.get('/',(req,res) => {
-res.send('Merhaba, to-do backend çalışıyor.');
+app.get('/', (req, res) => {
+res.send('Merhaba, To-Do backend çalışıyor.');
 });
-app.listen(300, () => {
+app.listen(3000, () => {
 console.log('Sunucu 3000 portunda çalışıyor.');
 });
-
-
-
-

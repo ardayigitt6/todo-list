ba97f8e3e364b4737918dbf30b890b27f0c75220
Arda Yiğit <arda.yigit.5811@gmail.com>
2025-06-05 04:46:55 +0300

    CRUD endpointleri tamamlandı, testler yapıldı

--- index.js
+++ index.js
@@ -20,3 +20,25 @@ res.send('Merhaba, To-Do backend çalışıyor.');
 app.listen(3000, () => {
 console.log('Sunucu 3000 portunda çalışıyor.');
 });
+app.put('/todos/:id', (req,res) =>{
+const {id} = req.params;    
+const {title} = req.body;
+const todo = todos.find(t => t.id==Number(id));
+if (!todo){
+return res.status(404).json({ error:'Todo bulunmadı!'})
+}
+if (!title) {
+return res.status(400).json({ error:'Başlık gerekli!'});
+}
+todo.title=title;
+res.json(todo);
+});
+app.delete ('/todos/:id', (req,res) => {
+const {id} = req.params;
+const index = todos.findIndex(t=> t.id==Number(id));
+if (index ==-1){
+return res.status(404).json({error: 'Todo bulunmadı !'});
+}
+todos.splice(index,1);
+res.json({message: 'Todo silindi.'});
+});
